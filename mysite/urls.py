35ccc186c2720b3a8aaf6be1745dad02ad5a0f3b
Joseph Schilz <joseph@schilz.org>
2018-07-06 01:28:17 -0700

    Follow lesson 7 instructions.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myblog.urls')),
+    path('login/', login, {'template_name': 'login.html'}, name="login"),
+    path('logout/', logout, {'next_page': '/'}, name="logout"),
 ]
